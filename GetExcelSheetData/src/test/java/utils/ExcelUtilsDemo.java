36e81d05831450074ecf2f0820a85d5505f77892
Sharad Khairnar <sharad.khairnar.sa@gmail.com>
2023-11-21 15:02:47 +0530

    How to use Excel for getting data in Selenium Java Framework

---
+++ GetExcelSheetData/src/test/java/utils/ExcelUtilsDemo.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class ExcelUtilsDemo {
+
+	@SuppressWarnings("static-access")
+	public static void main(String[] args) {
+		
+		String projectpath = System.getProperty("user.dir");
+		ExcelUtils excel=new ExcelUtils(projectpath+"/Excel/TestData.xlsx", "sheet1");
+		excel.getRowCount();
+		excel.getCellDataString(1, 0);
+		excel.getCellDataNumeric(1, 1);
+	}
+}
